bac0a80a38c0699da23d0c8fd1fd023b1e038acc
AbstractSoft <ghergu@yahoo.com>
2020-06-01 15:08:24 +0300

    Added error handling for file opening

--- Probleme/Fisiere/fisiere_1.cpp
+++ Probleme/Fisiere/fisiere_1.cpp
@@ -10,29 +10,29 @@ constexpr std::string_view nume_fisier_out = "b.txt";
 
 int main() {
 
-  std::size_t numar_1;
-  std::size_t numar_2;
-  std::size_t suma;
+    std::size_t numar_1;
+    std::size_t numar_2;
+    std::size_t suma;
 
-  std::ifstream fisier_numere_in(nume_fisier_in);
+    std::ifstream fisier_numere_in(nume_fisier_in);
 
-  // if (!fisier_numere_in) {
-  //   std::cerr << "Eroare la deschiderea fisierului!" << '\n';
-  // }
+    if (!fisier_numere_in) {
+        std::cerr << "Eroare la deschiderea fisierului!" << '\n';
+        exit(1);
+    }
 
+    fisier_numere_in >> numar_1;
+    std::cout << "Primul numar este: " << numar_1;
+    fisier_numere_in >> numar_2;
+    std::cout << "Al doilea numar este: " << numar_2;
 
-  fisier_numere_in >> numar_1;
-  std::cout << "Primul numar este: " << numar_1;
-  fisier_numere_in >> numar_2;
-  std::cout << "Al doilea numar este: " << numar_2;
+    fisier_numere_in.close();
 
-  fisier_numere_in.close();
+    suma = numar_1 + numar_2;
 
-  suma = numar_1 + numar_2;
+    std::ofstream fisier_numere_out(nume_fisier_out);
+    fisier_numere_out << numar_1 << ' '
+                      << numar_2;
 
-  std::ofstream fisier_numere_out(nume_fisier_out);
-  fisier_numere_out << numar_1 << ' '
-                    << numar_2;
-
-      return 0;
+    return 0;
 }
