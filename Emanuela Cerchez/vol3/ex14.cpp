72953048074bfaeb06334e70bed6b656323783ee
AbstractSoft <ghergu@yahoo.com>
2020-11-24 23:44:16 +0200

    Removed the graph related documents

---
+++ Emanuela Cerchez/vol3/ex14.cpp
@@ -0,0 +1,12 @@
+// Ex. 14, pag.: 13
+
+#include <iostream>
+
+int main() {
+
+  uint16_t lungime_secventa = 0;
+  std::cout << "Introduceti lungimea secventei de numere naturale: ";
+  std::cin >> lungime_secventa;
+
+  
+}
